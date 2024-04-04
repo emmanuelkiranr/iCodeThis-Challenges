fb11285502ca15169c31a01fdbc47d2b73e2ac4a
Emmanuel Kiran R <emmanuelkiranr@gmail.com>
2024-04-04 20:26:23 +0530

    polls fetched from api

--- Poll/scripts.js
+++ Poll/scripts.js
@@ -1,66 +1,3 @@
-const polls = [
-  {
-    id: 1,
-    text: "What is the capital of France?",
-    options: [
-      { id: 1, text: "London", checked: false },
-      { id: 2, text: "Berlin", checked: false },
-      { id: 3, text: "Paris", checked: false, answer: true },
-      { id: 4, text: "Rome", checked: false },
-    ],
-    skipped: false,
-    answered: false,
-  },
-  {
-    id: 2,
-    text: "Who is the author of 'To Kill a Mockingbird'?",
-    options: [
-      { id: 1, text: "Mark Twain", checked: false },
-      { id: 2, text: "Harper Lee", checked: false, answer: true },
-      { id: 3, text: "J.K. Rowling", checked: false },
-      { id: 4, text: "Charles Dickens", checked: false },
-    ],
-    skipped: false,
-    answered: false,
-  },
-  {
-    id: 3,
-    text: "What is the chemical symbol for water?",
-    options: [
-      { id: 1, text: "H2O", checked: false, answer: true },
-      { id: 2, text: "CO2", checked: false },
-      { id: 3, text: "NH3", checked: false },
-      { id: 4, text: "O2", checked: false },
-    ],
-    skipped: false,
-    answered: false,
-  },
-  {
-    id: 4,
-    text: "Which planet is known as the Red Planet?",
-    options: [
-      { id: 1, text: "Venus", checked: false },
-      { id: 2, text: "Saturn", checked: false },
-      { id: 3, text: "Jupiter", checked: false },
-      { id: 4, text: "Mars", checked: false, answer: true },
-    ],
-    skipped: false,
-    answered: false,
-  },
-  {
-    id: 5,
-    text: "Who painted the Mona Lisa?",
-    options: [
-      { id: 1, text: "Pablo Picasso", checked: false },
-      { id: 2, text: "Vincent van Gogh", checked: false },
-      { id: 3, text: "Leonardo da Vinci", checked: false, answer: true },
-      { id: 4, text: "Michelangelo", checked: false },
-    ],
-    skipped: false,
-    answered: false,
-  },
-];
-
 const question = document.querySelector(".question");
 const optionsContainer = document.querySelector(".options");
 const submit = document.querySelector("[data-submit]");
@@ -70,12 +7,25 @@ const modal = document.querySelector(".modal-container");
 const score = document.querySelector("[data-score]");
 const retry = document.querySelector("[data-retry]");
 
+let polls = [];
 let currentQuestionId = 1;
 let selectedQn = null;
 let selectedQnOpt = [];
 let currentSelctedOpt = null;
 let labelOpt = null;
 let points = 0;
+
+async function fetchPolls() {
+  await fetch(`https://mocki.io/v1/61aa7c4a-881a-48fc-abbc-2df12157db06`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      polls = data;
+      // localStorage.setItem("polls", JSON.stringify(data));
+    });
+  renderPoll();
+}
 
 function renderPoll() {
   getQuestion();
@@ -214,4 +164,4 @@ function clearElements(container) {
   }
 }
 
-renderPoll();
+fetchPolls();
